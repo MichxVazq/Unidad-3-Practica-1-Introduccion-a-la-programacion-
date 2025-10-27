62b30b89fcacdad738178ba8af06939ce1dce1d7
MichxVazq <l25580441@reynosa.tecnm.mx>
2025-10-26 22:50:37 -0500

    Add files via upload

---
+++ Practica 15/Practica 15/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace Practica_15
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            label3 = new Label();
+            textBox2 = new TextBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(34, 112);
+            label1.Name = "label1";
+            label1.Size = new Size(729, 17);
+            label1.TabIndex = 0;
+            label1.Text = "Dado como dato el sueldo de un trabajador, el programa aplica un aumento del 15% si el sueldo es inferior a 400000";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 27.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(78, 163);
+            label2.Name = "label2";
+            label2.Size = new Size(280, 50);
+            label2.TabIndex = 1;
+            label2.Text = "Ingresa Sueldo";
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Segoe UI", 26.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox1.Location = new Point(376, 163);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(259, 54);
+            textBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.ControlLight;
+            button1.Font = new Font("Segoe UI", 24F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.Location = new Point(131, 239);
+            button1.Name = "button1";
+            button1.Size = new Size(181, 59);
+            button1.TabIndex = 3;
+            button1.Text = "Calcular";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = SystemColors.ControlLight;
+            button2.Font = new Font("Segoe UI", 26.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button2.Location = new Point(411, 239);
+            button2.Name = "button2";
+            button2.Size = new Size(181, 59);
+            button2.TabIndex = 4;
+            button2.Text = "Borrar";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 27.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(78, 339);
+            label3.Name = "label3";
+            label3.Size = new Size(266, 50);
+            label3.TabIndex = 5;
+            label3.Text = "Nuevo Sueldo";
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("Segoe UI", 26.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox2.Location = new Point(376, 339);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(259, 54);
+            textBox2.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(textBox2);
+            Controls.Add(label3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox textBox1;
+        private Button button1;
+        private Button button2;
+        private Label label3;
+        private TextBox textBox2;
+    }
+}
